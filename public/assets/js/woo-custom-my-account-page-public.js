b6340c5e268d79755cd702b2fd8aa32d97a4afae
Pallavi Gupta <pallavi@wbcomdesigns.com>
2019-07-15 18:55:12 +0530

    Code changes

--- public/assets/js/woo-custom-my-account-page-public.js
+++ public/assets/js/woo-custom-my-account-page-public.js
@@ -1,32 +1,73 @@
-(function( $ ) {
-	'use strict';
+jQuery(document).ready(function($) {
+    "use strict";
 
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
+    function preload_popup() {
+        $('body').append( '<div id="wcmp-avatar-form-overlay" class="loading"></div>' );
+    }
 
-})( jQuery );
+    function center_popup() {
+        var p = $( '#wcmp-avatar-form');
+
+        if( ! p.length ) {
+            return false;
+        }
+
+        var window_w = $(window).width(),
+            window_h = $(window).height(),
+            o_width  = p.data('width'),
+            o_height = p.data('height'),
+            width    = ( ( window_w - 60 ) > o_width ) ? o_width : ( window_w - 60 ),
+            height   = ( ( window_h - 120 ) > o_height ) ? o_height : ( window_h - 120 );
+
+        p.css({
+            'left'   : (( window_w/2 ) - ( width/2 )),
+            'top'    : (( window_h/2 ) - ( height/2 )),
+            'width'  : width + 'px',
+            'height' : height + 'px'
+        });
+    }
+
+    function close_popup() {
+        $( '#wcmp-avatar-form, #wcmp-avatar-form-overlay' ).fadeOut('slow', function(){
+            $(this).remove();
+        });
+    }
+
+    $(window).on( 'resize', center_popup );
+
+    $('#load-avatar').click( function (ev) {
+        
+        ev.preventDefault();
+        preload_popup();
+
+        $.ajax({
+            url: wcmp.ajaxurl.toString().replace( '%%endpoint%%', wcmp.actionPrint ),
+            data: {},
+            dataType: 'html',
+            success: function( res ) {
+
+                $('body').append( res ).find('#wcmp-avatar-form-overlay').removeClass('loading');
+                center_popup();
+
+                $('#wcmp-avatar-form-overlay, i.close-form').click(function(){
+                    close_popup();
+                })
+            }
+        })
+        
+    });
+
+    $(document).on( "click", ".group-opener", function(ev){
+        ev.preventDefault();
+
+        var container = $(this).closest('li');
+
+        if( container.hasClass( 'is-tab' ) && $(window).width() >= 480 ) {
+            container.toggleClass( 'is-hover' );
+            return;
+        }
+
+        $(this).find('i.opener').toggleClass( 'fa-chevron-down' ).toggleClass( 'fa-chevron-up' );
+        $(this).next('.myaccount-submenu').slideToggle();
+    });
+});
