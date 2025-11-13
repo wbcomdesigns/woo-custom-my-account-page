301a155d9d1cc825dfcb8d0c333f35b8d689bcc8
Nitin Patil <pallavi@wbcomdesigns.com>
2025-11-13 11:00:07 +0530

    Update text and content
    https://3.basecamp.com/5798509/buckets/37614349/card_tables/cards/9275498331

--- admin/partials/wcmp-welcome-page.php
+++ admin/partials/wcmp-welcome-page.php
@@ -19,27 +19,27 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<div class="wbcom-welcome-content">
 			<div class="wbcom-welcome-support-info">
 				<h3><?php esc_html_e( 'Help &amp; Support Resources', 'woo-custom-my-account-page' ); ?></h3>
-				<p><?php esc_html_e( 'Here are all the resources you may need to get help from us. Documentation is usually the best place to start. Should you require help anytime, our customer care team is available to assist you at the support center.', 'woo-custom-my-account-page' ); ?></p>
+				<p><?php esc_html_e( 'If you need assistance, here are some helpful resources. Our documentation is a great place to start, and our support team is available if you require further help.', 'woo-custom-my-account-page' ); ?></p>
 				<div class="wbcom-support-info-wrap">
 					<div class="wbcom-support-info-widgets">
 						<div class="wbcom-support-inner">
 							<h3><span class="dashicons dashicons-book"></span><?php esc_html_e( 'Documentation', 'woo-custom-my-account-page' ); ?></h3>
-							<p><?php esc_html_e( 'We have prepared an extensive guide on WooCommerce Custom My Account Page to learn all aspects of the plugin. You will find most of your answers here.', 'woo-custom-my-account-page' ); ?></p>
+							<p><?php esc_html_e( 'Explore our detailed guide on Custom My Account Page for WooCommerce to understand all the features and how to make the most of them.', 'woo-custom-my-account-page' ); ?></p>
 							<a href="<?php echo esc_url( 'https://docs.wbcomdesigns.com/doc_category/woo-custom-my-account-page/' ); ?>" class="button button-primary button-welcome-support" target="_blank"><?php esc_html_e( 'Read Documentation', 'woo-custom-my-account-page' ); ?></a>
 						</div>
 					</div>
 					<div class="wbcom-support-info-widgets">
 						<div class="wbcom-support-inner">
 							<h3><span class="dashicons dashicons-sos"></span><?php esc_html_e( 'Support Center', 'woo-custom-my-account-page' ); ?></h3>
-							<p><?php esc_html_e( 'We strive to offer the best customer care via our support center. Once your theme is activated, you can ask us for help anytime.', 'woo-custom-my-account-page' ); ?></p>
+							<p><?php esc_html_e( 'Our support team is here to assist you with any questions or issues. Feel free to contact us anytime through our support center.', 'woo-custom-my-account-page' ); ?></p>
 							<a href="<?php echo esc_url( 'https://wbcomdesigns.com/support/' ); ?>" class="button button-primary button-welcome-support" target="_blank"><?php esc_html_e( 'Get Support', 'woo-custom-my-account-page' ); ?></a>
 						</div>
 					</div>
 					<div class="wbcom-support-info-widgets">
 						<div class="wbcom-support-inner">
 							<h3><span class="dashicons dashicons-admin-comments"></span><?php esc_html_e( 'Got Feedback?', 'woo-custom-my-account-page' ); ?></h3>
-							<p><?php esc_html_e( 'We want to hear about your experience with the plugin. We would also love to hear any suggestions you may for future updates.', 'woo-custom-my-account-page' ); ?></p>
-							<a href="<?php echo esc_url( 'https://wbcomdesigns.com/contact/' ); ?>" class="button button-primary button-welcome-support" target="_blank"><?php esc_html_e( 'Send Feedback', 'woo-custom-my-account-page' ); ?></a>
+							<p><?php esc_html_e( 'We\'d love to hear about your experience with the plugin. Your feedback and suggestions help us improve future updates.', 'woo-custom-my-account-page' ); ?></p>
+							<a href="<?php echo esc_url( 'https://wbcomdesigns.com/submit-review/ ' ); ?>" class="button button-primary button-welcome-support" target="_blank"><?php esc_html_e( 'Send Feedback', 'woo-custom-my-account-page' ); ?></a>
 						</div>
 					</div>
 				</div>
